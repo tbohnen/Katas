a1dbfb108b3221e5ef12b1da55feb87918110fa4
tbohnen <tbohnen.jnr@gmail.com>
2014-02-18 06:25:54 +0200

    Initial Roman Numerals Kata done with Janco Wolmarans
    This is the initial version that I paired up with Janco Wolmarans.

---
+++ RomanNumerals/Translator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RomanNumerals
+{
+    class Translator
+    {
+        public string Convert(int numberToConvert)
+        {
+            switch (numberToConvert)
+            {
+                case 1:
+                    return "I";
+                case 2:
+                    return "II";
+                default:
+                    {
+                        var convertedResult = "";
+
+                        int thousandResult = numberToConvert / 1000;
+                        convertedResult = GetRomanNumber(thousandResult, "M");
+
+                        numberToConvert = numberToConvert % 1000;
+                        int hundredResult = numberToConvert / 500;
+                        convertedResult = convertedResult + GetRomanNumber(hundredResult, "D");
+
+                        numberToConvert = numberToConvert % 500;
+                        hundredResult = numberToConvert / 100;
+                        convertedResult = convertedResult + GetRomanNumber(hundredResult, "C");
+
+                        return convertedResult;
+                    }
+            }
+        }
+
+        private static string GetRomanNumber(int thousandResult, string numeral)
+        {
+            string convertedResult = "";
+
+            for (int i = 1; i <= thousandResult; i++)
+            {
+                convertedResult = string.Concat(convertedResult, numeral);
+            }
+            return convertedResult;
+        }
+    }
+}
---
+++ RomanNumerals/TranslatorTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    [TestFixture]
+    class TranslatorTests
+    {
+
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(2000, "MM")]
+        [TestCase(2500, "MMD")]
+        [TestCase(2600, "MMDC")]
+        public void Translator_InstantiateTranslator(int numberToConvert, string expected)
+        {
+            var translator = new Translator();
+
+            var convertedNumeral = translator.Convert(numberToConvert);
+
+            Assert.AreEqual(expected, convertedNumeral);
+        }
+
+    }
+
+
+}
