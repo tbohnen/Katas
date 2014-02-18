0da7fa942a7d5fcbb9f4bcbc0c625f9c0cb22483
tbohnen <tbohnen.jnr@gmail.com>
2014-02-18 07:26:07 +0200

    Removed duplication by creating a list
    Removed the duplication by adding the roman numerals to a list with
    their corresponding decimal division number.  It needs to be
    re-factored...

--- RomanNumerals/Translator.cs
+++ RomanNumerals/Translator.cs
@@ -7,33 +7,53 @@ namespace RomanNumerals
 {
     class Translator
     {
+
+        private readonly Dictionary<string, int> _numeralDivisionList = new Dictionary<string, int>();
+        private int _numberToConvert = -1;
+
+        public Translator()
+        {
+            SetupNumeralDivisionQueue();
+        }
+
+        private void SetupNumeralDivisionQueue()
+        {
+            _numeralDivisionList.Add("M", 1000);
+            _numeralDivisionList.Add("D", 500);
+            _numeralDivisionList.Add("C", 100);
+            _numeralDivisionList.Add("L", 50);
+            _numeralDivisionList.Add("X", 10);
+            _numeralDivisionList.Add("I", 1);
+        }
+
+
         public string Convert(int numberToConvert)
         {
-            switch (numberToConvert)
+            _numberToConvert = numberToConvert;
+
+            var convertedResult = "";
+
+            while (_numeralDivisionList.Count > 0 && _numberToConvert != 0)
             {
-                case 1:
-                    return "I";
-                case 2:
-                    return "II";
-                default:
-                    {
-                        var convertedResult = "";
-
-                        int thousandResult = numberToConvert / 1000;
-                        convertedResult = GetRomanNumber(thousandResult, "M");
-
-                        numberToConvert = numberToConvert % 1000;
-                        int hundredResult = numberToConvert / 500;
-                        convertedResult = convertedResult + GetRomanNumber(hundredResult, "D");
-
-                        numberToConvert = numberToConvert % 500;
-                        hundredResult = numberToConvert / 100;
-                        convertedResult = convertedResult + GetRomanNumber(hundredResult, "C");
-
-                        return convertedResult;
-                    }
+                convertedResult = string.Concat(convertedResult, GetNextRomanNumeral());
             }
+
+            return convertedResult;
         }
+
+        private string GetNextRomanNumeral()
+        {
+            var nextDivision = _numeralDivisionList.OrderByDescending(n => n.Value).First();
+            int result = _numberToConvert / nextDivision.Value;
+
+            var convertedResult = GetRomanNumber(result, nextDivision.Key);
+
+            _numberToConvert = _numberToConvert % nextDivision.Value;
+            _numeralDivisionList.Remove(nextDivision.Key);
+
+            return convertedResult;
+        }
+
 
         private static string GetRomanNumber(int thousandResult, string numeral)
         {
