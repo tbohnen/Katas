1b3c8a4db21f80cd74cf201d8748cf2f89de2ce1
Theo Bohnen <tbohnen.jnr@gmail.com>
2014-04-12 18:37:12 +0200

    Updated the tests for the kata

--- ruby/FizzBuzz/FizzBuzz.rb
+++ ruby/FizzBuzz/FizzBuzz.rb
@@ -1,22 +1,57 @@
-def FizzBuzzPrinter number 
+class FizzBuzzPrinter 
 
+  def print_value number
 
-  if number % 3 == 0 and number % 5 == 0
-    puts "FizzBuzz"
+    if number % 3 == 0 and number % 5 == 0
+      "FizzBuzz"
+	    
+    elsif number % 3 == 0
+      "Fizz"
+	    
+    elsif  number % 5 == 0
+      "Buzz"
     
-  elsif number % 3 == 0
-    puts "Fizz"
-    
-  elsif  number % 5 == 0
-    puts "Buzz"
-    
-  else
-    puts number
+    else
+      number
+    end
+
   end
 end
 
-for i in 0..100
+describe("Fizz buzz output") do
 
-  FizzBuzzPrinter(i)
+	it "1 Prints 1" do
+		printer = FizzBuzzPrinter.new
 
+		converted_Value = printer.print_value 1
+
+		expect(converted_Value).to eq 1
+	end
+	
+	it "3 Prints Fizz" do
+          printer = FizzBuzzPrinter.new
+
+	  converted_value = printer.print_value 3
+
+	  expect(converted_value).to eq "Fizz"
+	end
+
+	it "5 Prints Buzz" do
+
+          printer = FizzBuzzPrinter.new
+
+	  converted_value = printer.print_value 5 
+
+	  expect(converted_value).to eq "Buzz"
+	end
+
+	it "15 Prints FizzBuzz" do
+
+          printer = FizzBuzzPrinter.new
+
+	  converted_value = printer.print_value 15 
+
+	  expect(converted_value).to eq "FizzBuzz"
+	end
 end
+
